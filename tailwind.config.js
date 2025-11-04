3f3b3cfcfec71d98ff1728d09337d4f50b5a5bc1
william guzman <willesgume@gmail.com>
2025-11-03 22:00:38 -0500

    commit v1

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx}"],
+   darkMode : 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
